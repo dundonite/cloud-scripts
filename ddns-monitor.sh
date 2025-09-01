e8e9f493fad748f5d678cdba5dba934aad2e5538
dundonite <49844302+dundonite@users.noreply.github.com>
2025-08-31 21:54:42 -0500

    Removed initial execution of ddns-update.sh

--- ddns-monitor.sh
+++ ddns-monitor.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-/usr/local/bin/ddns-update.sh
-
 /sbin/ip -6 monitor prefix dev eth0 | sed 's/.* //g' | while read newaddr; do
   if [ "$newaddr" == "[NEWADDR]" ]; then
     /usr/local/bin/ddns-update.sh
