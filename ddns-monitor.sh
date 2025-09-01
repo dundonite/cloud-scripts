a93ab3989e87cdbeb273ae9c7c4c0371308a867d
dundonite <49844302+dundonite@users.noreply.github.com>
2025-08-31 21:50:10 -0500

    Fix path to ddns-update.sh script

--- ddns-monitor.sh
+++ ddns-monitor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-/usr/local/sbin/ddns-update.sh
+/usr/local/bin/ddns-update.sh
 
 /sbin/ip -6 monitor prefix dev eth0 | sed 's/.* //g' | while read newaddr; do
   if [ "$newaddr" == "[NEWADDR]" ]; then
